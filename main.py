daf77e86c286539aa2c9b11f54d9acfefd2637fb
cophilot <philxsb@gmail.com>
2024-01-28 16:19:28 +0100

    v1.0.0

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
     if not FORCE and not has_changes():
         log("No changes detected!")
         log("Exiting...")
-        sys.exit(1)
+        sys.exit(0)
         
     save_projects()
     generate_banner(projects)
